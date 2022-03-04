a8864adae4aeb46db5676b98ccbd30336bb1d177
Makoto <makoto@emurasoft.com>
2022-03-03 21:19:11 -0800

    args

--- src/wserver.c
+++ src/wserver.c
@@ -1,26 +1,31 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include "request.h"
 #include "io_helper.h"
-
-char default_root[] = ".";
 
 // ./wserver [-d basedir] [-p port] [-t threads] [-b buffers] [-s schedalg]
 int main(int argc, char *argv[]) {
     int c;
-    char *root_dir = default_root;
+    char *root_dir = ".";
     int port = 10000;
     
-    while ((c = getopt(argc, argv, "d:p:")) != -1)
-	switch (c) {
-	case 'd':
-	    root_dir = optarg;
-	    break;
-	case 'p':
-	    port = atoi(optarg);
-	    break;
-	default:
-	    fprintf(stderr, "usage: wserver [-d basedir] [-p port] [-t threads] [-b buffers] [-s schedalg]\n");
-	    exit(1);
+    while ((c = getopt(argc, argv, "d:p:")) != -1) {
+		switch (c) {
+		case 'd': root_dir = optarg;
+			break;
+		case 'p': port = atoi(optarg);
+			break;
+		case 't':
+			break;
+		case 'b':
+			break;
+		case 's':
+			break;
+		default:
+			fprintf(stderr,
+					"usage: wserver [-d basedir] [-p port] [-t threads] [-b buffers] [-s schedalg]\n");
+			exit(1);
+		}
 	}
 
     // run out of this directory
@@ -28,7 +33,7 @@ int main(int argc, char *argv[]) {
 
     // now, get to work
     int listen_fd = open_listen_fd_or_die(port);
-    while (1) {
+    while (true) {
 		struct sockaddr_in client_addr;
 		int client_len = sizeof(client_addr);
 		int conn_fd = accept_or_die(listen_fd, (sockaddr_t *) &client_addr, (socklen_t *) &client_len);
